085b2bcdb4ef6e609b15c081f147f9233114bfa9
Sebastian Roth <sebastian.roth@gmail.com>
2019-05-23 19:03:40 +0100

    Ensure keepOn future finishes

--- ios/Classes/ScreenPlugin.m
+++ ios/Classes/ScreenPlugin.m
@@ -3,8 +3,8 @@
 @implementation ScreenPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"github.com/clovisnicolas/flutter_screen"
-            binaryMessenger:[registrar messenger]];
+                                   methodChannelWithName:@"github.com/clovisnicolas/flutter_screen"
+                                   binaryMessenger:[registrar messenger]];
   ScreenPlugin* instance = [[ScreenPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
@@ -25,6 +25,7 @@
   else if ([@"keepOn" isEqualToString:call.method]) {
     NSNumber *b = call.arguments[@"on"];
     [[UIApplication sharedApplication] setIdleTimerDisabled:b.boolValue];
+    result(nil);
   }
   else {
     result(FlutterMethodNotImplemented);
