cfb5740a81e5834254112b263f2ec8ade157b0c8
LaelLuo <laelluo@qq.com>
2020-06-09 10:31:14 +0800

    add miui support

--- android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
+++ android/src/main/java/flutter/plugins/screen/screen/ScreenPlugin.java
@@ -1,5 +1,6 @@
 package flutter.plugins.screen.screen;
 
+import android.content.res.Resources;
 import android.provider.Settings;
 import android.view.WindowManager;
 
@@ -14,63 +15,75 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  */
 public class ScreenPlugin implements MethodCallHandler {
 
-  private ScreenPlugin(Registrar registrar){
-    this._registrar = registrar;
-  }
-  private Registrar _registrar;
-
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "github.com/clovisnicolas/flutter_screen");
-    channel.setMethodCallHandler(new ScreenPlugin(registrar));
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    switch(call.method){
-      case "brightness":
-        result.success(getBrightness());
-        break;
-      case "setBrightness":
-        double brightness = call.argument("brightness");
-        WindowManager.LayoutParams layoutParams = _registrar.activity().getWindow().getAttributes();
-        layoutParams.screenBrightness = (float)brightness;
-        _registrar.activity().getWindow().setAttributes(layoutParams);
-        result.success(null);
-        break;
-      case "isKeptOn":
-        int flags = _registrar.activity().getWindow().getAttributes().flags;
-        result.success((flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) != 0) ;
-        break;
-      case "keepOn":
-        Boolean on = call.argument("on");
-        if (on) {
-          System.out.println("Keeping screen on ");
-          _registrar.activity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        }
-        else{
-          System.out.println("Not keeping screen on");
-          _registrar.activity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        }
-        result.success(null);
-        break;
-
-      default:
-        result.notImplemented();
-        break;
+    private ScreenPlugin(Registrar registrar) {
+        this._registrar = registrar;
     }
-  }
 
-  private float getBrightness(){
-    float result = _registrar.activity().getWindow().getAttributes().screenBrightness;
-    if (result < 0) { // the application is using the system brightness
-      try {
-        result = Settings.System.getInt(_registrar.context().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float)255;
-      } catch (Settings.SettingNotFoundException e) {
-        result = 1.0f;
-        e.printStackTrace();
-      }
+    private Registrar _registrar;
+
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "github.com/clovisnicolas/flutter_screen");
+        channel.setMethodCallHandler(new ScreenPlugin(registrar));
     }
-    return result;
-  }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        switch (call.method) {
+            case "brightness":
+                result.success(getBrightness());
+                break;
+            case "setBrightness":
+                double brightness = call.argument("brightness");
+                WindowManager.LayoutParams layoutParams = _registrar.activity().getWindow().getAttributes();
+                layoutParams.screenBrightness = (float) brightness;
+                _registrar.activity().getWindow().setAttributes(layoutParams);
+                result.success(null);
+                break;
+            case "isKeptOn":
+                int flags = _registrar.activity().getWindow().getAttributes().flags;
+                result.success((flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) != 0);
+                break;
+            case "keepOn":
+                Boolean on = call.argument("on");
+                if (on) {
+                    System.out.println("Keeping screen on ");
+                    _registrar.activity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                } else {
+                    System.out.println("Not keeping screen on");
+                    _registrar.activity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                }
+                result.success(null);
+                break;
+
+            default:
+                result.notImplemented();
+                break;
+        }
+    }
+
+    private float getBrightness() {
+        float result = _registrar.activity().getWindow().getAttributes().screenBrightness;
+        if (result < 0) { // the application is using the system brightness
+            try {
+                result = Settings.System.getInt(_registrar.context().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS) / (float) getBrightnessMax();
+            } catch (Settings.SettingNotFoundException e) {
+                result = 1.0f;
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    private int getBrightnessMax() {
+        try {
+            Resources system = Resources.getSystem();
+            int resId = system.getIdentifier("config_screenBrightnessSettingMaximum", "integer", "android");
+            if (resId != 0) {
+                return system.getInteger(resId);
+            }
+        } catch (Exception ignore) {
+        }
+        return 255;
+    }
 
 }
